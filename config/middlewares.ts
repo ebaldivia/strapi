509b8c72bd23959121fad814d050546495b59438
Emiliane Baldivia <emiliane@cobrefacil.com.br>
2025-10-10 11:15:48 -0300

    collection articles

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,6 +2,14 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'local-time-zone'],
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+        config: {
+            provider: 'local',
+            breakpoints: {
+                large: 1200,
+                medium: 750,
+                small: 500,
+                thumbnail: 245
+            },
+        },
+    },
+});
