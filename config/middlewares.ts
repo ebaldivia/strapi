559d667b96b4d6b96f6574396b4488e7bd9890af
Emiliane Baldivia <emiliane@cobrefacil.com.br>
2025-10-20 15:48:32 -0300

    config middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,31 +1,16 @@
-export default ({ env }) => [
+export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: [
-        'http://localhost:5173',
-        env('APP_URL'),
-      ],
+      origin: ['http://localhost:5173'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'local-time-zone'],
     },
   },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
