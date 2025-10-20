a85d8283efd174a0be6ba4f24b67f99ccb84e497
Emiliane Baldivia <emiliane@cobrefacil.com.br>
2025-10-20 15:18:02 -0300

    config server

--- config/server.ts
+++ config/server.ts
@@ -2,8 +2,14 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('APP_URL'),
-  proxy: true,
+  proxy: { koa: true },
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    url: env('PUBLIC_ADMIN_URL', '/'),
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', ''),
+    },
   },
 });
